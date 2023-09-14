247dd5174f419ca90eeca3e02fa12e4ede6b236a
07inam <144999511+07inam@users.noreply.github.com>
2023-09-14 13:55:24 +0530

    Create strem.py

---
+++ strem.py
@@ -0,0 +1,29 @@
+import pickle
+import streamlit as st
+
+# Load the model from the pickle file
+with open(r"C:\Users\91812\Desktop\Test jupyter\lr.pickle", 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Define the Streamlit app
+st.title('Sentiment Analysis App')
+
+# Create a text input field for user input
+review = st.text_area('Enter your review:')
+
+# Create a button to trigger the analysis
+analyze_button = st.button('Analyze')
+
+# Function to perform sentiment analysis
+def perform_sentiment_analysis(text):
+    prediction = model.predict([text])
+    sentiment = 'Positive' if prediction[0] == 1 else 'Negative'
+    return sentiment
+
+# Display the sentiment analysis result when the button is clicked
+if analyze_button:
+    if review:
+        sentiment = perform_sentiment_analysis(review)
+        st.write(f'Sentiment: {sentiment}')
+    else:
+        st.warning('Please enter a review before analyzing.')
