1eab60de04c3b2e4c329bd35bb1fe9851307dff8
07inam <144999511+07inam@users.noreply.github.com>
2023-09-14 14:22:36 +0530

    Update strem.py

--- strem.py
+++ strem.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # Load the model from the pickle file
-with open("lr.pickle", 'rb') as model_file:
+with open("classifier.pickle", 'rb') as model_file:
     model = pickle.load(model_file)
 
 # Define the Streamlit app
